e8b05122226629326590c707d79de4b727334b75
Stephanie <70037508+Stephanie01011011@users.noreply.github.com>
2021-05-08 16:41:17 -0400

    Added text file, letterGuess(), and getWordList()
    Added what I worked on so far so other can see and determine what they need to put in their sections. getWordList() will be important for whomever is working on chooseWord.

---
+++ wordListAndletterGuess.py
@@ -0,0 +1,45 @@
+# def createCanvas():
+    #text box field(blah blah , command = letterguess())
+#def chooseWord():
+    #get word
+    #wordArray = []
+    #asteriskArray = []
+def getWordList():
+    file = open('wordList.txt', 'r')
+    wordArray = []
+    i = file
+    for words in i.read().split():
+        wordArray.append(words)
+    
+            
+        
+    
+    
+
+def letterGuess(textBox, wordArray, asteriskArray):
+    found = False
+    countGuess = 0
+    userInput = textBox.get()
+    for el in wordArray:
+        if userInput == el:
+            found = True
+        else:
+            countGuess += 1
+    if found == True:
+        asteriskArray[countGuess] = textBox.get()
+    else:
+        missedGuesses += 1
+        drawHouse()
+
+getWordList()
+            
+    
+        
+
+
+    
+    
+    
+    
+    
+            
